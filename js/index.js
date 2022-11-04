c39c17e80ebba17eeb49b4c1f73c47977af006f6
ELGHAZOUI Fawzi <fawzie.pro@gmail.com>
2022-11-05 00:25:06 +0100

    :sparkles: Header improvements

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-let buttonLanguage = document.getElementById('hulcButton')
+let buttonLanguage = document.getElementById("hulcButton");
+let bgLanguage = document.getElementById("hLanguages");
+let textLanguage = document.getElementById("hlUl");
+let expHeader = document.getElementById("hultuExp");
+let bgHeader = document.getElementById("hExp");
 
-buttonLanguage.onclick() = function () {
-  
+buttonLanguage.onclick = function () {
+  if (bgLanguage.style.height == 0) {
+    bgLanguage.style.height = "92px";
+    textLanguage.style.opacity = "1";
+  }
 }
+
+expHeader.onclick = function () {
+  bgHeader.style.opacity = "1";
+}
+
+
+/*z-index: 89;
+position: fixed;
+top: 145px;
+right: 5px;
+display: flex;
+background-color: #1D1F22;
+opacity: 0.9;
+*/
--- php/header.php
+++ php/header.php
@@ -3,22 +3,147 @@
     <li id="huLiLogo"><img id="hullLogo" src="../images/logo/logo.png"></li>
     <li id="huLiTabs">
       <ul id="hultUl">
-        <li style="margin:auto 25px auto 0 !important;"><a href="">Le ministère</a></li>
+        <li style="margin:auto 25px auto 0 !important;"><p>Le ministère</p></li>
         <li><a href="">Nous rejoindre</a></li>
-        <li><a href="">Nos expertises</a></li>
+        <li><p id="hultuExp">Nos expertises</p></li>
         <li><a href="">Salle de presse</a></li>
         <li><a href="">Contact</a></li>
       </ul>
     </li>
     <li id="huLiChangelang">
-      <button id="hulcButton" type="button"><p>FR</p></button>
+      <button id="hulcButton" type="button">
+        <ul>
+          <li><p>FR</p></li>
+          <li><p>›</p></li>
+        </ul>
+      </button>
     </li>
   </ul>
 </header>
 <div id="hLanguages">
-  <ul>
+  <ul id="hlUl">
     <li><a href="">Français</a></li>
     <li><a href="">English</a></li>
-    <li><a href="">Deutsch</a></li>
+    <!--li><a href="">Deutsch</a></li-->
+  </ul>
+</div>
+<div id="hExp">
+  <ul id="heUl">
+
+    <li id="heuLi">
+      <ul class="heulUl">
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+      </ul>
+    </li>
+
+    <li id="heuLi">
+      <ul class="heulUl">
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+      </ul>
+    </li>
+
+    <li id="heuLi">
+      <ul class="heulUl">
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+      </ul>
+    </li>
+
+    <li id="heuLi">
+      <ul class="heulUl">
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+        <li class="heuluLi"><div class="heululDiv">
+          <ul class="heululdUl">
+            <li style="margin: auto 5px;"><img src="../images/icons/maille.png"></li>
+            <li style="margin: auto auto auto 0;"><p>Design and</br>construction</p></li>
+          </ul>
+        </div></li>
+      </ul>
+    </li>
   </ul>
 </div>
